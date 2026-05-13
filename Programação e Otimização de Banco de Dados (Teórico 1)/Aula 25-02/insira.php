a31f2c72e64fcdc6a2325d8674b30752bf84de8e
Jefferson H. Santos <210017@upf.br>
2026-05-12 23:43:10 -0300

    alterado nome da pasta para Programação e Otimização de Banco de Dados (Teórico 1)

---
+++ Programação e Otimização de Banco de Dados (Teórico 1)/Aula 25-02/insira.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Atividade_01</title>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <form action="resultado.php" method="get">
+        <p>
+            Insira um Valor: <input type="text" name="valor1">
+        </p>
+        <p>
+            Insira um segundo Valor: <input type="text" name="valor2">
+        </p>
+        <p>
+            <input type="submit" value="Enviar">
+        </p>
+    </form>
+</body>
+
+
+
+</html>
+
